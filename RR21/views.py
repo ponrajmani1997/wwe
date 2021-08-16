a39be44e80fa3b13b9ab2adb761e55ba552627c3
ponraj mani <ponrajmani1997@gmail.com>
2021-08-16 11:44:04 +0530

    first commit

---
+++ RR21/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def RUMBLE21(request):
+    return HttpResponse('EDGE is the winner of RUMBLE21')
